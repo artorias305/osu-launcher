248abc8ffec193186a59cfa8428a627d8129c2c6
Artorias <105161738+artorias305@users.noreply.github.com>
2023-07-05 00:17:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import flet
-from flet import Page, Column, Row, Container, Text, Stack, TextField, Image, ElevatedButton, BottomSheet, IconButton
-from flet import icons, dropdown, colors, padding, alignment, border_radius, theme
+from flet import Page, ElevatedButton, BottomSheet, TextField, FloatingActionButton, Switch
 import os
 import subprocess
 
 def main(page: Page):
     page.title = "Osu! Launcher"
     appdata_path = os.getenv('LOCALAPPDATA')
-    
+    page.horizontal_alignment = flet.CrossAxisAlignment.CENTER
+    page.vertical_alignment = flet.MainAxisAlignment.CENTER
+    page.window_width = "630"
+    page.window_height = "270"
+
     def get_osu_path():
         osu_path = os.path.join(appdata_path, 'osu!')
         return os.path.join(osu_path, 'osu!.exe')
@@ -26,8 +29,7 @@ def main(page: Page):
 
     def launch_private_server(serverip, *args):
         osu_exe = get_osu_path()
-        subprocess.run([osu_exe, f"-devserver", serverip])
-        save_server_ip(serverip)
+        subprocess.run([osu_exe, "-devserver", serverip])
 
     def read_server_ip():
         try:
@@ -36,7 +38,7 @@ def main(page: Page):
                 return serverip.strip()
         except FileNotFoundError:
             return ""
-        
+
     def save_server_ip(serverip):
         with open("server_ip.txt", "w") as file:
             file.write(serverip)
@@ -45,35 +47,51 @@ def main(page: Page):
         bs.open = True
         bs.update()
 
-    def close_settings(e):
+    def close_settings(serverip):
         bs.open = False
+        save_server_ip(serverip)
         bs.update()
 
-    txt_server_ip = TextField(hint_text="Enter server ip")
+    def change_theme(e):
+        page.theme_mode = (
+            flet.ThemeMode.DARK
+            if page.theme_mode == flet.ThemeMode.LIGHT
+            else flet.ThemeMode.LIGHT
+        )
+        mode.label = (
+            "Light theme" if page.theme_mode == flet.ThemeMode.LIGHT else "Dark Theme"
+        )
+        page.update()
+
+    def close(*args):
+        page.window_destroy()
+
+    page.theme_mode = flet.ThemeMode.DARK
+    mode = Switch(label="Dark theme", on_change=change_theme)
+
+    txt_server_ip = TextField(hint_text="Enter server IP (e.g. akatsuki.pw)")
     bs = BottomSheet(
-        Container(
-        Column(
-            [
+        flet.Container(
+            flet.Column([
                 txt_server_ip,
-                ElevatedButton("Close", on_click=close_settings),
-            ],
-            tight=True,
-        ),
-        padding=10,
+                flet.ElevatedButton("Confirm", on_click=lambda event: close_settings(txt_server_ip.value)),
+            ], tight=True),
+            padding=10,
         ),
         open=False,
     )
 
     page.overlay.append(bs)
-    page.add(IconButton(icon=icons.SETTINGS, on_click=open_settings))
+    page.add(FloatingActionButton(icon=flet.icons.SETTINGS, on_click=open_settings))
 
     btn_private_server = ElevatedButton(text="Launch Private Server", on_click=lambda event: launch_private_server(txt_server_ip.value))
     btn_osu = ElevatedButton(text="Launch OSU!", on_click=launch_osu)
     btn_lazer = ElevatedButton(text="Launcher OSU! Lazer", on_click=launch_lazer)
     serverip = read_server_ip()
     txt_server_ip.value = serverip
+    close_btn = flet.OutlinedButton(text="Close", on_click=close)
 
-    page.add(btn_osu, btn_lazer, btn_private_server)
-    
+    page.add(mode, btn_osu, btn_lazer, btn_private_server, close_btn)
+
 
 flet.app(target=main)
